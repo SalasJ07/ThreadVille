8591b86700eff1c5a1fcb4ad7242126eb8b6ef5f
Rodri2210eh <naliuska.espher@gmail.com>
2022-05-22 14:33:29 -0600

    agregando cambios al codigo de interfaz

--- src/animation/interface.rs
+++ src/animation/interface.rs
@@ -9,7 +9,6 @@ let mut configuration: *mut config;
  */
 void create_interface(){
 
-
   // Inicializacion de ncurses (necesario)
   let mut x: i64 = 0;
   let mut y: i64 = 0;
@@ -22,26 +21,26 @@ void create_interface(){
 
   // Puntero temporal que nos ayudara a recorrer la lista de monitores
   let mut temp_monitor: *mut monitor_info = (*mut monitor_info) malloc(sizeof(monitor_info));
-  temp_monitor = configuration -> monitors_list -> head;
+  temp_monitor = configuration.monitors_list.head;
 
   // Se recorre la lista de monitores en config
 	while(temp_monitor != NULL){
 
       // Se crea un nuevo window por cada monitor y se guarda en la estructura
-      temp_monitor -> canvas_window = newwin(temp_monitor -> height_canvas_size,temp_monitor -> width_canvas_size, y, x);
+      temp_monitor.canvas_window = newwin(temp_monitor.height_canvas_size,temp_monitor.width_canvas_size, y, x);
       
       // Se posicionan en la terminal segun el ancho de cada uno
-      x += temp_monitor -> width_canvas_size;
+      x += temp_monitor.width_canvas_size;
       
-      //y+=temp_monitor -> height_canvas_size;
+      //y+=temp_monitor.height_canvas_size;
       
       // Funcion de ncurses para pintar los bordes del window
-      box(temp_monitor -> canvas_window, 0, 0);
+      box(temp_monitor.canvas_window, 0, 0);
       
       // Es necesario para mostrar los cambios hechos en window
-      wrefresh(temp_monitor-> canvas_window);
+      wrefresh(temp_monitor.canvas_window);
 		  
       // Se mueve al siguiente
-      temp_monitor = temp_monitor -> next;
+      temp_monitor = temp_monitor.next;
 	}
 }
