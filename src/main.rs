384301f49e46952ce90cf35965f11e833632e185
Rodri2210eh <naliuska.espher@gmail.com>
2022-05-22 20:01:08 -0600

    agregando ejemplo de corrida

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-mod "../my_pthread/my_pthread.rs"
-use crate::my_pthread::my_pthread::*;
+#[path = "./my_pthread/my_pthread.rs"] mod my_pthread;
 
+fn multiXdos(numero:i64) {
+    numero = numero * 2
+}
 
 fn main() {
     
     // Se llama a la función encargada de crear el thread
-    //my_thread_create();
+    my_pthread::my_thread_create(multiXdos, 2, 1, 1);
+    
+    my_pthread::run_all_threads()
 
 }
