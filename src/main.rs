00eace5b0d969bf6e47b5fd2f384937664d483e2
SalasJ07 <ate.salas20@gmail.com>
2022-05-22 20:31:49 -0500

    changes in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod "../my_pthread/my_pthread.rs"
 use crate::my_pthread::my_pthread::*;
 
+
 fn main() {
     
     // Se llama a la función encargada de crear el thread
-    my_thread_create();
+    //my_thread_create();
 
 }
--- src/my_pthread/animation_processor.rs
+++ src/my_pthread/animation_processor.rs
@@ -1,21 +1,10 @@
-//extern crate ncurses;
-
 use ncurses::*;
 
 /*
-    ncurses funciona para aplicaciones de consola, tambien se 
-    pueden cambiar colores o mover el cursor
-*/
+    Estructuras de datos que pueden servir para el procesamiento
+    de las figuras y animaciones
+ */
 
-
-// Estructuras de datos que pueden servir para el procesamiento
-// de las figuras y animaciones
-
-
-/*
-    Structs en Rust:
-    https://doc.rust-lang.org/book/ch05-01-defining-structs.html
-*/
 
 // Para guardar la informacion de la pantalla en la que desplegar animaciones
 struct screen_data {
@@ -29,7 +18,6 @@ struct screen_data {
 
 
 // Para tener una serie de pantallas e ir cambiandolas
-
 struct screen_queue {
     let head: *mut screen_data,
     let tail: *mut screen_data,
@@ -38,7 +26,6 @@ struct screen_queue {
 
 
 // Informacion que puede tener una animacion
-
 struct animation_data {
     sched: u32,
     screen_num: u32,
@@ -50,15 +37,13 @@ struct animation_data {
     starting_y: u32,
     ending_x: u32,
     ending_y: u32,
-    // -
 }
 
-
+// Estructura a mostrar durante la ejecución
 struct layout {
     let p: *mut String,
     total_screens: u32,
     let list: mut* screen_queue,
-    // -
 }
 
 fn animation_processor() {
