e1356ae5b609363afb32666d45acf116224629f0
SalasJ07 <ate.salas20@gmail.com>
2022-05-22 19:50:17 -0500

    include files problem

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use crate::my_pthread::my_pthread;
+mod "../my_pthread/my_pthread.rs"
+use crate::my_pthread::my_pthread::*;
 
 fn main() {
     
     // Se llama a la función encargada de crear el thread
-    my_pthread::my_thread_create();
+    my_thread_create();
 
 }
